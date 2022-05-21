2b6d5f2599129e26240616ecca0c7222d86de9ae
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-21 13:06:32 +0500

    Min-Max, While, BinarySearch

---
+++ Array_Algorithms/Binary_Search_Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Program {
+
+    static Boolean BinarySearch(int[] inputArray, int item) {
+        int min = 0;
+        int max = inputArray.Length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (item == inputArray[mid]) {
+                return true;
+            } else if (item < inputArray[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(BinarySearch(arr, 5));
+    }
+}
