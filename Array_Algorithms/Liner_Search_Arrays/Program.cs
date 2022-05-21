926207e46cf01b539d1f661d7ee2b590c1f75cde
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-21 13:03:21 +0500

    Array.Find,Array.ForEach

---
+++ Array_Algorithms/Liner_Search_Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Program {
+
+    static Boolean LinearSearch(int[] input, int n) {
+        foreach(int current in input) {
+            if (n == current) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void Main(string[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6 };
+        // Console.WriteLine(LinearSearch(arr, 4));
+        // Console.WriteLine(LinearSearch(arr, 8));
+
+        int item = Array.Find(arr, element => element == 3);
+        Console.WriteLine(item);
+        int[] items = Array.FindAll(arr, element => element >= 5);
+        Array.ForEach(items, Console.WriteLine);
+    }
+}
