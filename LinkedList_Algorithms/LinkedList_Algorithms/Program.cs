9bca012d6e3de22af7090b1fc493c1e581db69d3
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-22 20:40:19 +0500

    Node,next,head,current,displayContents,deleteBackHalf,displayContents

---
+++ LinkedList_Algorithms/LinkedList_Algorithms/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+class CustomLinkedList {
+
+    Node head;
+    public class Node {
+        public int data;
+        public Node next;
+        public Node(int d) { data = d; }
+    }
+
+    public void deleteBackHalf() {
+
+        if (head == null || head.next == null) {
+            head = null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+        Node prev = null;
+
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+    }
+
+    public void displayContents() {
+        Node current = head;
+
+        while(current != null) {
+            Console.Write(current.data + "->");
+            current = current.next;
+        }
+    }
+
+    static void Main(string[] args) {
+        CustomLinkedList linkedList = new CustomLinkedList();
+        Node firstNode = new Node(3);
+        Node secondNode = new Node(4);
+        Node thirdNode = new Node(5);
+        Node fourthNode = new Node(6);
+
+        linkedList.head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = fourthNode;
+
+        linkedList.displayContents();
+        linkedList.deleteBackHalf();
+        Console.WriteLine();
+        linkedList.displayContents();
+    }
+}
