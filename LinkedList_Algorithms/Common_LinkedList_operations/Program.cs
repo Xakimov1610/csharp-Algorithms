366fd3337eed6267484b95c46989f0b8ea32cf61
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-22 11:28:44 +0500

    AddLast,Contains,Count,RemoveFirst

---
+++ LinkedList_Algorithms/Common_LinkedList_operations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program {
+
+    static void Main(string[] args) {
+        LinkedList<string> listy = new LinkedList<string>();
+        // AddLast
+        // AddFirst
+        listy.AddLast("Sarah");
+        listy.AddLast("Polly");
+        listy.AddLast("Rebecca");
+        listy.AddLast("Jess");
+        listy.AddLast("Jackie");
+
+        Console.WriteLine(listy.Contains("Polly"));
+        Console.WriteLine(listy.Count);
+
+        listy.RemoveFirst();
+        
+        foreach(string item in listy) {
+            Console.Write(item + "->");
+        }
+
+        Console.WriteLine();
+    }
+}
