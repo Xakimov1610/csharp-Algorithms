1ca29048e2307d652f9bc736ecc2d07833fd8e69
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 20:16:50 +0500

    Node,HashSet,hasCycle

---
+++ Algorithms_for_HashBased_Structures/Detect_a_Cyclic_LinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿class CustomLinkedList {
+    Node head;
+
+        public class Node {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public Boolean hasCycle() {
+        HashSet<Node> nodes = new HashSet<Node>();
+
+        Node current = head;
+        while (current != null) {
+            if (nodes.Contains(current)) {
+                return true;
+            } else {
+                nodes.Add(current);
+            }
+
+            current = current.next;
+        }
+        return false;
+    }
+
+    static void Main(string[] args) {
+        CustomLinkedList noCycleLinkedList = new CustomLinkedList();
+        Node firstNode = new Node(3);
+        Node secondNode = new Node(4);
+        Node thirdNode = new Node(5);
+        Node fourthNode = new Node(6);
+
+        noCycleLinkedList.head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = fourthNode;
+
+        Console.WriteLine(noCycleLinkedList.hasCycle());
+
+        CustomLinkedList cycleLinkedList = new CustomLinkedList();
+        cycleLinkedList.head = firstNode;
+        thirdNode.next = secondNode;
+
+        Console.WriteLine(cycleLinkedList.hasCycle());
+    }
+}
