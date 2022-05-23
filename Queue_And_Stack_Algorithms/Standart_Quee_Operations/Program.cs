e8ad7955c54dfe766f0ce4eb881247b2b0a267bf
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 16:50:58 +0500

    Enqueue, Dequeue,Peek,TryDequeue(out current)

---
+++ Queue_And_Stack_Algorithms/Standart_Quee_Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Queue<int> queue = new Queue<int>(); // FIFO
+
+        queue.Enqueue(1);
+        queue.Enqueue(8);
+        queue.Enqueue(28);
+        queue.Enqueue(44);
+        queue.Enqueue(88);
+        queue.Enqueue(55);
+
+        int removeItem1 = queue.Dequeue();
+
+        foreach(var item in queue)
+        {
+            System.Console.WriteLine($"Queue items : {item}");
+        }
+        System.Console.WriteLine();
+        System.Console.WriteLine($"Removed item 1 : {removeItem1}");
+        System.Console.WriteLine($"Removed item 2 : {queue.Dequeue()}");
+
+        System.Console.WriteLine($"First Item queue : {queue.Peek()}");
+
+
+        int current;
+
+        while(queue.TryDequeue(out current)) // Agar Dequeue qilsa True , Agar qilmasa False qaytaradi
+        {
+            System.Console.WriteLine(current);
+        }
+    }
+}
