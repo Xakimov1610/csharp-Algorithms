f37ea503bdd369581926ea3810bed3ece5db6ec6
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 17:37:11 +0500

    Push,Pop,Peek,TryPeek(out item)

---
+++ Queue_And_Stack_Algorithms/Basic_Stack_operations/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Stack<string> stack = new Stack<string>();
+
+        System.Console.WriteLine("Start Main");
+        stack.Push("Main");
+
+        System.Console.WriteLine("Start ResponseBuilder");
+        stack.Push("ResponseBuilder");
+
+        System.Console.WriteLine("Start CallExternalService");
+        stack.Push("CallExternalService");
+
+        System.Console.WriteLine("End : " + stack.Pop());
+
+        System.Console.WriteLine("Start ParseExternalData");
+        stack.Push("ParseExternalData");
+
+        System.Console.WriteLine("End : " + stack.Pop());
+        System.Console.WriteLine("End : " + stack.Pop());
+        System.Console.WriteLine("End : " + stack.Pop());
+
+        // stack.Peek();
+        // stack.TryPeek();
+        string item;
+        System.Console.WriteLine(stack.TryPeek(out item)); // ››› False
+    }
+}
