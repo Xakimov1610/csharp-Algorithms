4d0519b5f0937b1950e04d579db6d586ff763eee
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 18:30:55 +0500

    Matching ››)()()‹‹, stack.Push,Pop

--- Queue_And_Stack_Algorithms/Matching_Parentheses/Program.cs
+++ Queue_And_Stack_Algorithms/Matching_Parentheses/Program.cs
@@ -23,8 +23,42 @@
         }
         return stack.Count == 0;
     }
+
+    static Boolean hasMatchingSymbolTracker(String s)
+    {
+        int matchingSmybolTracker = 0;
+        for (int c = 0; c < s.Length; c++)
+        {
+            char current = s[c];
+            if (current == '(')
+            {
+                matchingSmybolTracker++;
+                continue;
+            }
+            if (current == ')')
+            {
+                matchingSmybolTracker--;
+                if (matchingSmybolTracker < 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return matchingSmybolTracker == 0;
+    }
     public static void Main(string[] args)
     {
-       System.Console.WriteLine(hasMatchingParentheses("((hello () world))"));
+       System.Console.WriteLine(hasMatchingParentheses("((hello()))"));    // true
+       System.Console.WriteLine(hasMatchingParentheses("(()(hello())"));   // false
+       System.Console.WriteLine(hasMatchingParentheses("((hello))"));      // true
+       System.Console.WriteLine(hasMatchingParentheses("(hello)"));        // true
+       System.Console.WriteLine();
+       System.Console.WriteLine(hasMatchingParentheses(")hello("));        // false
+       System.Console.WriteLine(hasMatchingParentheses("())hello)"));      // false
+       System.Console.WriteLine(hasMatchingParentheses("hello(("));        // false
+       System.Console.WriteLine(hasMatchingParentheses("(hello"));         // false
+       System.Console.WriteLine();
+       System.Console.WriteLine(hasMatchingSymbolTracker("(hello()))"));   // False
+       System.Console.WriteLine(hasMatchingSymbolTracker("hello()"));      // True
     }
 }
