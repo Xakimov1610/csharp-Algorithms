a27d4857f162804af5a16f86d1b1843114e20fd5
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 17:14:01 +0500

    printBinary,En-Dequeue,current * 10 + 1

---
+++ Queue_And_Stack_Algorithms/Queue_Generate_Binary_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    static void pritBinary(int n)
+    {
+        if(n <= 0)
+            return;
+        
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(1);
+
+        for (int i = 0; i < n; i++)
+        {
+            int current = queue.Dequeue();
+            System.Console.WriteLine(current); 
+            queue.Enqueue(current * 10);
+            queue.Enqueue(current * 10 + 1);
+        }
+        System.Console.WriteLine();
+    }
+    public static void Main(string[] args)
+    {
+       pritBinary(5); // ››› > > ?
+       pritBinary(-2); // ››› > > ?
+    }
+}
