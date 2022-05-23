75d1528eb810c23e4bf77ea6620fdae4e2905c51
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 20:04:05 +0500

    Dict,KeyValuePair, listdagi elementlar necha marta qaytarilgan ?

---
+++ Algorithms_for_HashBased_Structures/Use_The_Dictionary_type/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program {
+
+    static void displayFreqOfEachElement(int[] arr) {
+        Dictionary<int, int> freqDictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < arr.Length; i++) {
+            if (freqDictionary.ContainsKey(arr[i])) {
+                freqDictionary[arr[i]]++;
+            } else {
+                freqDictionary[arr[i]] = 1;
+            }
+        }
+
+        foreach(KeyValuePair<int, int> x in freqDictionary) {
+            Console.WriteLine(x.Key + " -> " + x.Value);
+        }
+    }
+
+    static void Main(string[] args) {
+        displayFreqOfEachElement(new int[] 
+            {3, 0, 2, 4, 7, 3, 4, 5, 7, 6, 7});
+    }
+}
