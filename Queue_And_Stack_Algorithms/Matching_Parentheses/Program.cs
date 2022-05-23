d5619ae2bbdd39cc8025fcfe9fecb29824b8a8b5
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-23 18:12:36 +0500

    Matching ››)()()‹‹, stack.Push,Pop

---
+++ Queue_And_Stack_Algorithms/Matching_Parentheses/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    static Boolean hasMatchingParentheses(String s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+        }
+        return stack.Count == 0;
+    }
+    public static void Main(string[] args)
+    {
+       System.Console.WriteLine(hasMatchingParentheses("((hello () world))"));
+    }
+}
