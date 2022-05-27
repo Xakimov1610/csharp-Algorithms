5cba64f7cf456c8fc36ed67b99c351570a6fa01f
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-28 02:22:28 +0500

    changed

--- Searching_Sorting_Alogrithms/Program.cs
+++ Searching_Sorting_Alogrithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Searching_Sorting_Algorithms.Searching;
+using Searching_Sorting_Algorithms.Sorting;
 
 public class Program
 {
@@ -6,8 +7,10 @@ public class Program
     {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int number = 10;
-        var a = new BinarySearch();
-        var b = 
-        Console.WriteLine(a);
+        SelectionSort.Sort(arr);
+        Console.WriteLine(BinarySearch.Search(arr, number));
+        Console.WriteLine(BinarySearch.RecursionSearch(arr, 0, arr.Length - 1, number));
+    
+        
     }
 }
