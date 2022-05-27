cdd9d03a5e10df6fbb51f2b8b2f2656b98ff4260
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-05-28 01:48:21 +0500

    Way 2

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -18,43 +18,94 @@
         // }
 
         
-        // QuickSort(arr, 0, arr.Length-1);
+        // QuickSort_1_Way(arr, 0, arr.Length-1);
+
+        QuickSort(arr, 0, arr.Length-1);
         System.Console.WriteLine(string.Join(",", arr));
 
     }
-    
-    // static int[] QuickSort(int[] arr, int low, int high)
-    // {
-    //     int pivot = arr[(low + high) / 2];
-    //     int i = low;
-    //     int j = high;
-    //     while (i <= j)
-    //     {
-    //         while (arr[i] < pivot)
-    //         {
-    //             i++;
-    //         }
-    //         while (arr[j] > pivot)
-    //         {
-    //             j--;
-    //         }
-    //         if (i <= j)
-    //         {
-    //             int temp = arr[i];
-    //             arr[i] = arr[j];
-    //             arr[j] = temp;
-    //             i++;
-    //             j--;
-    //         }
-    //     }
-    //     if (low < j)
-    //     {
-    //         QuickSort(arr, low, j);
-    //     }
-    //     if (i < high)
-    //     {
-    //         QuickSort(arr, i, high);
-    //     }
-    //     return arr;
-    // }
+    private static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+    {
+        if (minIndex >= maxIndex)
+        {
+            return array;
+        }
+
+        int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+
+        QuickSort(array, minIndex, pivotIndex - 1);
+
+        QuickSort(array, pivotIndex + 1, maxIndex);
+
+        return array;
+    }
+
+    public static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+    {
+        int pivot = minIndex - 1;
+
+        for (int i = minIndex; i <= maxIndex; i++)
+        {
+            if (array[i] < array[maxIndex])
+            {
+                pivot++;
+                Swap(ref array[pivot], ref array[i]);
+            }
+        }
+
+        pivot++;
+        Swap(ref array[pivot], ref array[maxIndex]);
+
+        return pivot;
+    }
+
+    public static void Swap(ref int leftItem, ref int rightItem)
+    {
+        int temp = leftItem;
+
+        leftItem = rightItem;
+
+        rightItem = temp;
+    }
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+    public static int[] QuickSort_1_Way(int[] arr, int low, int high)
+    {
+        int pivot = arr[(low + high) / 2];
+        int i = low;
+        int j = high;
+        while (i <= j)
+        {
+            while (arr[i] < pivot)
+            {
+                i++;
+            }
+            while (arr[j] > pivot)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j)
+        {
+            QuickSort_1_Way(arr, low, j);
+        }
+        if (i < high)
+        {
+            QuickSort_1_Way(arr, i, high);
+        }
+        return arr;
+    }
 }
+
+    
